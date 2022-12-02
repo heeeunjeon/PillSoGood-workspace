41a49097a4cb1c30c506592802a2e472c4178ffe
Gwajang <cktkdgh3@gmail.com>
2022-12-02 13:56:00 +0900

    자바버전 수정 - 상호
    자바버전수정

--- PillSoGood_Project/src/main/java/com/kh/pill/notice/model/service/NoticeServiceImpl.java
+++ PillSoGood_Project/src/main/java/com/kh/pill/notice/model/service/NoticeServiceImpl.java
@@ -1,5 +1,46 @@
 package com.kh.pill.notice.model.service;
 
-public class NoticeServiceImpl {
+import java.util.ArrayList;
+
+import com.kh.pill.common.model.vo.PageInfo;
+import com.kh.pill.notice.model.vo.Notice;
+
+public class NoticeServiceImpl implements NoticeService {
+
+	@Override
+	public int selectListCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<Notice> selectList(PageInfo pi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int increaseCount(int boardNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Notice selectBoard(int boardNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int deleteBoard(int boardNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateBoard(Notice n) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
