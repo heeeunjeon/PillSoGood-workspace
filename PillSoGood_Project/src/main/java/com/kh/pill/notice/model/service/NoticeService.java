3ce942669d156899efe5c17c388f8a14c632198f
Gwajang <cktkdgh3@gmail.com>
2022-12-02 13:31:39 +0900

    템플릿 추가 - 상호
    템플릿 추가

--- PillSoGood_Project/src/main/java/com/kh/pill/notice/model/service/NoticeService.java
+++ PillSoGood_Project/src/main/java/com/kh/pill/notice/model/service/NoticeService.java
@@ -1,5 +1,26 @@
 package com.kh.pill.notice.model.service;
 
-public interface NoticeService {
+import java.util.ArrayList;
 
+import com.kh.pill.common.model.vo.PageInfo;
+import com.kh.pill.notice.model.vo.Notice;
+
+public interface NoticeService {
+	
+	int selectListCount();
+	
+
+	ArrayList<Notice> selectList(PageInfo pi);
+	
+
+	int increaseCount(int boardNo);
+	
+	Notice selectBoard(int boardNo);
+	
+
+	int deleteBoard(int boardNo);
+	
+
+	int updateBoard(Notice n);
+	
 }
